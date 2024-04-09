cd3d6f709c82a6516150a6536f4d0948b7cdad8c
Sʜɪᴠᴀᴍ ꭙ Jᴀɪsᴡᴀʟ <jaiswalshivam72319@gmail.com>
2024-04-09 15:58:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,12 +14,12 @@ API_HASH = getenv("e581880ff82e201ce3dd007f06384074")
 BOT_TOKEN = getenv("5953802361:AAGI-QOMhKYOwfqzFmR-NJDc3TcKMQ2ylz4")
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("MONGO_DB_URI", mongodb+srv://shivam0:shivam098@shivam0.yu9flzq.mongodb.net/?retryWrites=true&w=majority&appName=shivam0)
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", -1001605285063))
+LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 5242142514))
